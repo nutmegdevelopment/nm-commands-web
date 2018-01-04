17dfdf7b12a84c307f1562aea5f7f4c2a317b618
Shaun Stone <shauns@nutmeg.com>
2018-01-04 15:14:48 +0000

    seperated tasks

---
+++ commands/run-nut-services.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const shell = require('shelljs');
+const { spawn } = require('child_process');
+const process = require('process');
+
+// Start up services
+shell.cd('~/nutmeg/nutmeg-services');
+const servicesProcess = spawn('bash', ['start-min-services.sh'], { shell: true, stdio: 'inherit' });
